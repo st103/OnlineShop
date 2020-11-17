aee705253b14dd89e358b255a8f081aaf4d6896a
DanicaMassie <S11810002@student.unklab.ac.id>
2020-11-17 10:58:27 +0800

    payment

--- app/src/main/java/com/example/onlineshop/alfamart.java
+++ app/src/main/java/com/example/onlineshop/alfamart.java
@@ -9,6 +9,6 @@ public class alfamart extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_alfamart);
+        setContentView(R.layout.activity_alfamart2);
     }
 }
